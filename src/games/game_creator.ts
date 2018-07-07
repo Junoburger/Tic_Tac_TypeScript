790af8852cb02ae677aadea1708669c931dc79c8
JunoJulius <yunoyulius@gmail.com>
2018-07-07 11:57:36 +0200

    added core logic i.e game_creator

---
+++ src/games/game_creator.ts
@@ -0,0 +1,16 @@
+export const moves = (board1, board2) =>
+  board1
+    .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
+    .reduce((a, b) => a.concat(b)).length;
+
+const existingColors = ["blue", "red", "green", "magenta", "yellow"];
+
+export const randomColor = () => {
+  return existingColors[Math.floor(Math.random() * existingColors.length)];
+};
+
+export const validColor = newColor => {
+  if (existingColors.includes(newColor)) return newColor;
+};
+
+export const defaultBoard = [["o", "o", "o"], ["o", "o", "o"], ["o", "o", "o"]];
